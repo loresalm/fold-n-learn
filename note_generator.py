382263199a4478311c3fb9b4cc9f0b92fc2164ec
loresalm <salmina.lorenzo@outlook.com>
2025-03-05 16:30:42 +0100

    general working code baseline

---
+++ note_generator.py
@@ -0,0 +1,14 @@
+import json
+import res.utils as ut
+
+if __name__ == "__main__":
+    """
+    config_file_path = 'data/inputs/config.json'
+    output_file_path = 'data/output/foldable_daybook.pdf'
+    with open(config_file_path, 'r') as json_file:
+        config = json.load(json_file)
+    start_date = config["start_date"]
+    nb_weeks = config["nb_weeks"]
+    """
+    output_file_path = 'data/foldable_daybook.pdf'
+    ut.generate_pdf(output_file_path)
